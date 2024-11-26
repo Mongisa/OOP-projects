48ce924eaff9cf9a84b3d9d9a2a8706c7413624a
Alessandro Monte <46656617+Mongisa@users.noreply.github.com>
2024-11-26 17:23:53 +0100

    Implemented .csv import/export

--- MyStudentListIZ/src/mystudentlistiz/MyStudentListViewController.java
+++ MyStudentListIZ/src/mystudentlistiz/MyStudentListViewController.java
@@ -14,9 +14,14 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.*;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class MyStudentListViewController implements Initializable {
 
@@ -83,5 +88,68 @@ public class MyStudentListViewController implements Initializable {
     @FXML
     public void delStudent(ActionEvent event) {
 
+    }
+
+    @FXML
+    public void loadAction(ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Selezione file .csv da importare");
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV","*.csv"));
+
+        File file = fc.showOpenDialog(new Stage());
+
+        if(file == null) return;
+
+        readCSV(file);
+    }
+
+    @FXML
+    public void saveAction(ActionEvent event) {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Seleziona dove salvare .csv");
+        fc.setInitialFileName("mystudentslist");
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV","*.csv"));
+
+
+        File path = fc.showSaveDialog(new Stage());
+
+        if(path == null) return;
+
+        saveCSV(path);
+    }
+
+    public void saveCSV(File path) {
+        try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path)))) {
+            pw.println("NOME;COGNOME;MATRICOLA");
+
+            for(Student s : students) {
+                pw.append(s.getName()).append(';');
+                pw.append(s.getSurname()).append(';');
+                pw.append(s.getCode());
+                pw.append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readCSV(File file) {
+        try(Scanner s = new Scanner(new BufferedReader(new FileReader(file)))) {
+            s.useDelimiter("[;\n]");
+            s.useLocale(Locale.ITALY);
+
+            if(s.nextLine() == null) return;
+
+            while(s.hasNext()) {
+                String nome = s.next();
+                String cognome = s.next();
+                String matricola = s.next();
+
+                Student st = new Student(nome,cognome,matricola);
+                students.add(st);
+            }
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
